ce0c407f0700a18b13a1a5fab9935038c12d9df9
hyperbel <hyperbel@users.noreply.github.com>
2024-08-11 17:19:04 +0200

    fix build nav function

--- setup.sh
+++ setup.sh
@@ -11,13 +11,17 @@ OUT_FILE=$OUT_DIR/$FILENAME
 build_nav() {
     CONTENT=$(ls $CONTENT_DIR | awk -F"." '{ print $1 " " }')
 
-	echo '<nav>\n<ul>' > $OUT_DIR/$NAV_FILENAME
-	echo '</ul>\n</nav>' >> $OUT_DIR/$NAV_FILENAME
+	echo -e \\t\<nav\>\\n\\t\\t\<ul\> > $OUT_DIR/$NAV_FILENAME
+	for value in ${CONTENT[@]}
+	do
+		echo -e "\t\t\t<li\><a href="/$value.html"\>$value</a\></li\>" >> $OUT_DIR/$NAV_FILENAME
+	done
+	echo -e \\t\\t\</ul\>\\n\\t\</nav\> >> $OUT_DIR/$NAV_FILENAME
 }
 
 
 build_nav && cat head.html.part \
-		$OUT_DIR/$NAV_FILE_NAME > $OUT_FILE
+		$OUT_DIR/$NAV_FILENAME > $OUT_FILE
 
 if [ ! -z $2 ]; then
 	cat content/$1.html >> $OUT_FILE
