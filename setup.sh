e8a5b20d5fed8ba08690c643de6265616e7a0a84
hyperbel <hyperbel@users.noreply.github.com>
2024-08-11 16:57:47 +0200

    create build_nav function

--- setup.sh
+++ setup.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
 OUT_DIR=out
+FILENAME=test.html
+NAV_FILENAME=nav.html.part
+CONTENT_DIR=content
 
-cat head.html.part \
-	nav.html.part \
-	footer.html.part \
+
+OUT_FILE=$OUT_DIR/$FILENAME
+
+build_nav() {
+    CONTENT=$(ls $CONTENT_DIR | awk -F"." '{ print $1 " " }')
+
+	echo '<nav>\n<ul>' > $OUT_DIR/$NAV_FILENAME
+	echo '</ul>\n</nav>' >> $OUT_DIR/$NAV_FILENAME
+}
+
+
+build_nav && cat head.html.part \
+		$OUT_DIR/$NAV_FILE_NAME > $OUT_FILE
+
+if [ ! -z $2 ]; then
+	cat content/$1.html >> $OUT_FILE
+fi
+
+cat footer.html.part \
 	script.html.part \
-	end.html.part > $OUT_DIR/test.html
+	end.html.part >> $OUT_FILE
