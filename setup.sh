dffcc79a56b45cf4b2eb1a4ddc7301b757055e25
hyperbel <hyperbel@users.noreply.github.com>
2024-08-11 19:09:40 +0200

    fix setup script

--- setup.sh
+++ setup.sh
@@ -9,25 +9,45 @@ STRUCTURE_DIR=structure
 
 OUT_FILE=$OUT_DIR/$FILENAME
 
-build_nav() {
-    CONTENT=$(ls $CONTENT_DIR | awk -F"." '{ print $1 " " }')
+if [ ! -z $1 ]; then
+    if [ ! -f $CONTENT_DIR/$1.html ]; then
+        echo "file $1.html not found"
+        exit 1
+    fi
+fi
 
+if [ ! -z $1 ]; then
+	echo "input file: $1"
+	OUT_FILE=$OUT_DIR/$1.html
+fi
+
+echo "output file: $OUT_FILE"
+
+CONTENT=$(ls $CONTENT_DIR | awk -F"." '{ print $1 " " }')
+
+build_nav() {
 	echo -e \\t\<nav\>\\n\\t\\t\<ul\> > $OUT_DIR/$NAV_FILENAME
 	for value in ${CONTENT[@]}
 	do
-		echo -e "\t\t\t<li\><a href="/$value.html"\>$value</a\></li\>" >> $OUT_DIR/$NAV_FILENAME
+		echo -e "\t\t\t<li\><a href="/$value.html"\>$value</a\></li\>" | sed 's/\\>/>/g' >> $OUT_DIR/$NAV_FILENAME
 	done
 	echo -e \\t\\t\</ul\>\\n\\t\</nav\> >> $OUT_DIR/$NAV_FILENAME
 }
 
 
+
 build_nav && cat $STRUCTURE_DIR/head.html.part \
 		$OUT_DIR/$NAV_FILENAME > $OUT_FILE
 
-if [ ! -z $2 ]; then
+if [ ! -z $1 ]; then
 	cat content/$1.html >> $OUT_FILE
 fi
 
 cat $STRUCTURE_DIR/footer.html.part \
 	$STRUCTURE_DIR/script.html.part \
 	$STRUCTURE_DIR/end.html.part >> $OUT_FILE
+
+if [ ! -z $1 ]; then
+    firefox $OUT_FILE &
+    disown -a
+fi
